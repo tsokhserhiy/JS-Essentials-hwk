bb60cb7c03efda1cd6a92a31c7b708f5294c945c
TsokhSerhiy <ser-cokh@yandex.ru>
2016-12-05 01:54:04 +0200

    added Task1

---
+++ Task1/js/script.js
@@ -0,0 +1,6 @@
+var name = prompt('Please, enter your name: ');
+var event = prompt('Please, enter event:', 'meeting');
+var time = prompt('Please, enter time:', '17.00');
+var place = prompt('Please, enter plase:', 'Lviv');
+
+console.log(name + ', has a ' + event + ', today at ' + time + ' somewhere in ' + place +'.');
