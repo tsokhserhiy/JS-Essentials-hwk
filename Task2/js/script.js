da4d17a669e62b54c5fc9883f3dd5fa6ecb7a5ba
TsokhSerhiy <ser-cokh@yandex.ru>
2016-12-05 02:40:15 +0200

    added Task2

---
+++ Task2/js/script.js
@@ -0,0 +1,6 @@
+var x1 = 5;
+var y1 = 5;
+var x2 = prompt('Please, enter x2:');
+var y2 = prompt('Please, enter y2:');
+var result = (Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)));
+alert ('The distance between points A(' + x1 + ',' + y1 + ') and B(' + x2 + ',' + y2 + ') is ' + result +'.');
