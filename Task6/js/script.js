c03be5f8b8834e735afc31a6516244e100ab27c3
TsokhSerhiy <ser-cokh@yandex.ru>
2016-12-10 03:05:09 +0200

    fixes bug

--- Task6/js/script.js
+++ Task6/js/script.js
@@ -1,16 +1,27 @@
-var userNumber = prompt("Please, enter your number!");
-var randomNumber = Math.round(Math.random() * 100);
-
-while (true) {
-
-  if (userNumber < randomNumber) {
-    userNumber = prompt("Our number is higher, try again");
+function startGame() {
+  var randomNumber = Math.round(Math.random() * 100);
+  function go() {
+    var userNumber = prompt("Please, enter your number!");
+    if (typeof(userNumber) == 'string') {
+      var userNumber = parseInt(userNumber);
+      if (isNaN(userNumber) == false) {
+        if (userNumber < randomNumber) {
+          alert("Our number is higher, try again");
+          go();
+        }
+        else if (userNumber > randomNumber) {
+          alert("Our number is lower, try again");
+          go();
+        }
+        else {
+          alert("You are right, the number is " + randomNumber);
+        }
+      } else {
+          alert('You have entered incorect value. Please try again');
+          go();
+        }
+    }
   }
-  else if (userNumber > randomNumber) {
-    userNumber = prompt("Our number is lower, try again");
-  }
-  else {
-    alert("You are right, the number is " + randomNumber);
-    break;
-  }
+  go();
 }
+startGame();
